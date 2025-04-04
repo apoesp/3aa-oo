d2f91d98a123e2076826c5d7c746e16b211da8f6
ariadna <ariadna22042004@gmail.com>
2025-04-04 16:47:30 +0200

    Affichage du menu avec un Polymon adversaire choisi au hasard

--- Menu.cpp
+++ Menu.cpp
@@ -2,18 +2,19 @@
 
 void Menu::afficherMenu() {
 
-    std::vector<Polymon> polymons = Storage::getInstance()->getList();
+    std::vector<Polymon> polymons = Storage::getInstance()->getList(); 
+    Polymon polymonAdverse = Storage::getInstance()->pickRandom();
 
-    std::cout << "Choisis ton Polymon :\n";
+    std::cout << "Choisis ton Polymon :\n\n";
 
     for (size_t i = 0; i < polymons.size(); i++) {
-        std::cout << i + 1 << polymons[i].getName() << std::endl;
+        std::cout << i + 1 << " " << polymons[i].getName() << std::endl;
     }
 
     int choix = 0;
 
-    std::cout << "Entrez le numéro de votre choix : ";
+    std::cout << "\nEntrez le numéro de votre choix : ";
     std::cin >> choix;    
 
-    std::cout << "Tu as choisi " << polymons[choix - 1].getName() << std::endl;
+    std::cout << "\nTu as choisi " << polymons[choix - 1].getName() << "\nTu joues contre " << polymonAdverse.getName() << std::endl;
 }
